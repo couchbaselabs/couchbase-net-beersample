03d804c45cf0ce5c64eb4ec913f6001a92565f76
jeffrymorris <jeffrymorris@gmail.com>
2014-09-02 16:33:01 -0700

    Added solution and project files

---
+++ Src/Couchbase.BeerSample.Domain/Geo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Couchbase.BeerSample.Domain
+{
+    public class Geo
+    {
+        public string Accuracy { get; set; }
+
+        [JsonProperty("lat")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("lon")]
+        public string Longitude { get; set; }
+        /*
+         * "geo": {
+            "accuracy": "ROOFTOP",
+            "lat": 37.7825,
+            "lon": -122.393
+         * */
+    }
+}
---
+++ Src/Couchbase.BeerSample.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Couchbase.BeerSample.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Couchbase.BeerSample.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35ca0b14-81ad-4dca-aac3-c1509bdeb519")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
